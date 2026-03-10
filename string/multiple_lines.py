df1595bfb734c3245e6afb0703c2b7f53516203c
vanessaruama <vanessaruama@gmail.com>
2026-03-09 22:04:16 -0300

    slicing e multiple lines

---
+++ string/multiple_lines.py
@@ -0,0 +1,18 @@
+nome = "Vanessa"
+
+# Escreve a mensagem utilizando uma string de múltiplas linhas (triple quotes) e interpolação de string (f-string),
+# respeitando a formatação e as quebras de linha
+mensagem = f"""Olá, me chamo {nome},
+estou aprendendo a trabalhar com strings em Python."""
+print(mensagem)
+
+print("""
+**************** MENU ****************
+      1 = Depositar
+      2 - Sacar
+      0 - Sair
+
+**************************************
+
+      Obrigada por usar nosso sistema!
+""")
---
+++ string/slicing.py
@@ -0,0 +1,21 @@
+nome = "Vanessa Ruama"
+
+# Fatiando a string para obter um caractere específico
+print(nome[0])
+
+# Fatiando a string para obter um intervalo de caracteres
+print(nome[0:9])
+
+# Fatiando a string para obter um intervalo de caracteres com um passo específico
+print(nome[10:])
+
+print(nome[10:16])
+
+# Fatiando a string com uso de passo para obter caracteres em posições específicas e pular outros caracteres
+print(nome[10:16:2])
+
+# Fatiando a string sem especificar o índice de início e o índice de fim para obter a string completa
+print(nome[:])
+
+# Fatiando a string com um passo negativo para obter a string invertida
+print(nome[::-1])
